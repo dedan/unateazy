0a6951d948e1036b9c4cb22f91ccb53c558d05d7
Stephan Gabler <stephan.gabler@gmail.com>
2010-07-07 11:38:35 +0200

    fixed intendation in works_controller.rb
    removed db psw in database.yml
    
    changed environment to development
    
    added the logfile to gitignore

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,45 +2,47 @@ class WorksController < ApplicationController
 
   before_filter :authenticate, :only => [:admin,:edit,:update,:create, :destroy]
 
+  # how many related works are shown below the current work
   N_RELATED = 5
+  
+  
   # GET /works GET /works.xml
-
-
   def show_text
-	 @work = Work.find(params[:id])
+    @work = Work.find(params[:id])
   end
 
-
+  #show the index page 
+  # TODO with a random work
   def index
-	 
+   
   end
 
   def admin
-	 @works = Work.find(:all)
+   @works = Work.find(:all)
   end
 
   def thumb
-	 @work = Work.find(params[:id])
-	 render :inline => "@work.operate {|p| p.resize '100x100'}", :type => :flexi
+   @work = Work.find(params[:id])
+   render :inline => "@work.operate {|p| p.resize '100x100'}", :type => :flexi
   end
 
   def mid_thumb
-	 @work = Work.find(params[:id])
-	 render :inline => "@work.operate {|p| p.resize '120x120'}", :type => :flexi
+   @work = Work.find(params[:id])
+   render :inline => "@work.operate {|p| p.resize '120x120'}", :type => :flexi
   end
 
 
   # GET /works/1 GET /works/1.xml
   def show
-	 if params[:id].nil?
-		@work = Work.find(:all).sort_by { rand }.first
-	 else
-		@work = Work.find(params[:id])
-	 end
-	 @rel_works = @work.rel_works.first(N_RELATED) unless @work.nil?
+   if params[:id].nil?
+    @work = Work.find(:all).sort_by { rand }.first
+   else
+    @work = Work.find(params[:id])
+   end
+   @rel_works = @work.rel_works.first(N_RELATED) unless @work.nil?
 
     respond_to do |format|
-		format.jpg if params[:format] == 'jpg'  
+    format.jpg if params[:format] == 'jpg'  
       format.html # index.html.erb
     end
   end
@@ -58,7 +60,7 @@ class WorksController < ApplicationController
   # GET /works/1/edit
   def edit
     @work = Work.find(params[:id])
-	 @others = Work.find(:all) - [@work]
+   @others = Work.find(:all) - [@work]
   end
 
   # POST /works POST /works.xml
@@ -81,16 +83,16 @@ class WorksController < ApplicationController
   def update
     @work = Work.find(params[:id])
 
-	 params[:checkbox].each do |box|
-		logger.debug "message  #{box[1].to_i == 1}"
-		logger.debug "message  #{Work.find(box[0])}"
-		tmp = Work.find(box[0].to_i)
-		if box[1].to_i == 1
-		  @work.rel_works << tmp unless @work.rel_works.include?(tmp)
-		else
-		  @work.rel_works.delete(tmp)
-		end
-	 end
+   params[:checkbox].each do |box|
+    logger.debug "message  #{box[1].to_i == 1}"
+    logger.debug "message  #{Work.find(box[0])}"
+    tmp = Work.find(box[0].to_i)
+    if box[1].to_i == 1
+      @work.rel_works << tmp unless @work.rel_works.include?(tmp)
+    else
+      @work.rel_works.delete(tmp)
+    end
+   end
 
     respond_to do |format|
       if @work.update_attributes(params[:work])
@@ -98,11 +100,11 @@ class WorksController < ApplicationController
         format.html { redirect_to(@work) }
         format.xml  { head :ok }
       else
-		@others = Work.find(:all) - [@work]
+    @others = Work.find(:all) - [@work]
         format.html { render :action => "edit" }
         format.xml  { render :xml => @work.errors, :status => :unprocessable_entity }
       end
-    end	
+    end 
   end
 
   # DELETE /works/1 DELETE /works/1.xml
@@ -119,9 +121,9 @@ class WorksController < ApplicationController
 
 private
   def authenticate
-	 authenticate_or_request_with_http_basic do |name, psw|
-		name == "matan" && psw == "test"
-	 end
+   authenticate_or_request_with_http_basic do |name, psw|
+    name == "matan" && psw == "test"
+   end
   end
 
 
