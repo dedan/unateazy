41574786bd33945679d5480eeeb68547f9873d49
Stephan Gabler <stephan.gabler@gmail.com>
2010-07-08 18:22:17 +0200

    cleaned up and commented the works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -20,52 +20,37 @@ class WorksController < ApplicationController
     @works = Work.find(:all)
   end
 
-  # def thumb
-  #  @work = Work.find(params[:id])
-  #  render :inline => "@work.operate {|p| p.resize '100x100'}", :type => :flexi
-  # end
-  # 
-  # def mid_thumb
-  #  @work = Work.find(params[:id])
-  #  render :inline => "@work.operate {|p| p.resize '120x120'}", :type => :flexi
-  # end
-
-
-  # GET /works/1 GET /works/1.xml
+  # display works
   def show
+    # if now work id is given, show a random work
     if params[:id].nil?
       @work = Work.find(:all).sort_by { rand }.first
     else
       @work = Work.find(params[:id])
     end
     
+    # determine whether the shadowbox plugin is needed (large image)
     dimensions        = ImageSpec::Dimensions.new(@work.image.path(:display).downcase)
     @shadowbox_needed = dimensions.width > 600 || dimensions.height > 600
     @rel_works        = @work.rel_works.first(N_RELATED) unless @work.nil?
 
     respond_to do |format|
-      format.jpg if params[:format] == 'jpg'  
-      format.html # index.html.erb
+      format.html
     end
   end
 
-  # GET /works/new GET /works/new.xml
+  # GET /works/new 
   def new
     @work = Work.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @work }
-    end
   end
 
   # GET /works/1/edit
   def edit
-    @work = Work.find(params[:id])
-   @others = Work.find(:all) - [@work]
+    @work   = Work.find(params[:id])
+    @others = Work.find(:all) - [@work]
   end
 
-  # POST /works POST /works.xml
+  # POST /works 
   def create
     @work = Work.new(params[:work])
 
@@ -73,10 +58,8 @@ class WorksController < ApplicationController
       if @work.save
         flash[:notice] = 'Work was successfully created.'
         format.html { redirect_to(@work) }
-        format.xml  { render :xml => @work, :status => :created, :location => @work }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @work.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -85,26 +68,24 @@ class WorksController < ApplicationController
   def update
     @work = Work.find(params[:id])
 
-   params[:checkbox].each do |box|
-    logger.debug "message  #{box[1].to_i == 1}"
-    logger.debug "message  #{Work.find(box[0])}"
-    tmp = Work.find(box[0].to_i)
-    if box[1].to_i == 1
-      @work.rel_works << tmp unless @work.rel_works.include?(tmp)
-    else
-      @work.rel_works.delete(tmp)
+    params[:checkbox].each do |box|
+      logger.debug "message  #{box[1].to_i == 1}"
+      logger.debug "message  #{Work.find(box[0])}"
+      tmp = Work.find(box[0].to_i)                  # number of box
+      if box[1].to_i == 1                           # is this box checked
+        @work.rel_works << tmp unless @work.rel_works.include?(tmp)
+      else
+        @work.rel_works.delete(tmp)
+      end
     end
-   end
 
     respond_to do |format|
       if @work.update_attributes(params[:work])
         flash[:notice] = 'Work was successfully updated.'
         format.html { redirect_to(@work) }
-        format.xml  { head :ok }
       else
-    @others = Work.find(:all) - [@work]
+        @others = Work.find(:all) - [@work]
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @work.errors, :status => :unprocessable_entity }
       end
     end 
   end
@@ -116,17 +97,15 @@ class WorksController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to("/login") }
-      format.xml  { head :ok }
     end
   end
 
 
 private
   def authenticate
-   authenticate_or_request_with_http_basic do |name, psw|
-    name == "matan" && psw == "test"
-   end
+    authenticate_or_request_with_http_basic do |name, psw|
+      name == "matan" && psw == "test"
+    end
   end
-
 
 end
