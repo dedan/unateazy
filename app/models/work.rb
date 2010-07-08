3e44740d839823cce749023774f4a25ff7cf5404
Stephan Gabler <stephan.gabler@gmail.com>
2010-07-08 18:01:45 +0200

    cleaned up the work model
    only paperclip is now used for attachments,
    all the fleximage stuff removed at all
    
    removed the thumbnail from the model, from now on the
    image of a work will be mandatory.
    When work is an image, thumbnail will be created from
    it, when work is a video or so, the image will be
    displayed as thumbnail
    
    Signed-off-by: Stephan Gabler <stephan.gabler@gmail.com>

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,45 +1,37 @@
 class Work < ActiveRecord::Base
   has_and_belongs_to_many  :works,
-	 :join_table => 'works_rel_works',
-	 :foreign_key => 'rel_work_id',
-	 :association_foreign_key => 'work_id',
-	 :after_add => :create_reverse_association,
-	 :after_remove => :remove_reverse_association
+   :join_table => 'works_rel_works',
+   :foreign_key => 'rel_work_id',
+   :association_foreign_key => 'work_id',
+   :after_add => :create_reverse_association,
+   :after_remove => :remove_reverse_association
 
   validates_presence_of :headline,  :description
+  
+  # Paperclip plugin (used for upload stuff and thumbnail)
+  has_attached_file :image, :styles => { 
+    :display=> "900x440",                                     # normal size view
+    :thumb => "80x80#" }                                      # thumbnail view
+  validates_attachment_content_type :image, 
+    :content_type => ['image/jpg', 'image/jpeg', 'image/gif', 'image/png']
 
-#  acts_as_fleximage do
-#      image_directory   'public/images/works'
-#      image_storage_format :jpg
-#  end
-	has_attached_file :image, :styles => { :display=> "900x440"}
-	validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'image/gif', 'image/png']
-	has_attached_file :thumbnail, :styles => { :original=> "80x80#"}
-	validates_attachment_content_type :thumbnail, :content_type => ['image/jpg', 'image/jpeg', 'image/gif', 'image/png']
-
+  # give related works
   def rel_works
     self.works
   end
 
+  # is the current work related to another one?
   def related_to(other)
-	 self.works.include?(other)
+   self.works.include?(other)
   end
 
-#  def image_file=(input_data)
-#	 unless input_data.is_a? String
-#		self.file_name	 = input_data.original_filename
-#		self.content_type = input_data.content_type.chomp
-#		self.binary_data	 = input_data.read
-#	 end
-#  end
-
-
+  # make the relation symmetric
   private
   def create_reverse_association(associated_work)
-	 associated_work.rel_works << self unless associated_work.rel_works.include?(self)
+   associated_work.rel_works << self unless associated_work.rel_works.include?(self)
   end
 
   def remove_reverse_association(associated_work)
-	 associated_work.rel_works.delete(self) if associated_work.rel_works.include?(self)
+   associated_work.rel_works.delete(self) if associated_work.rel_works.include?(self)
   end
 end
--- db/migrate/20090219152702_add_paperclip.rb
+++ db/migrate/20090219152702_add_paperclip.rb
@@ -4,10 +4,6 @@ class AddPaperclip < ActiveRecord::Migration
 	  add_column  :works, :image_content_type, :string
 	  add_column  :works, :image_updated_at, :timestamp
 	  add_column  :works, :image_file_size, :integer
-	  add_column  :works, :thumbnail_file_name, :string
-	  add_column  :works, :thumbnail_content_type, :string
-	  add_column  :works, :thumbnail_updated_at, :timestamp
-	  add_column  :works, :thumbnail_file_size, :integer
   end
 
   def self.down
@@ -15,9 +11,5 @@ class AddPaperclip < ActiveRecord::Migration
 	  remove_column  :works, :image_content_type      
 	  remove_column  :works, :image_updated_at        
 	  remove_column  :works, :image_file_size         
-	  remove_column  :works, :thumbnail_file_name     
-	  remove_column  :works, :thumbnail_content_type  
-	  remove_column  :works, :thumbnail_updated_at    
-	  remove_column  :works, :thumbnail_file_size     
   end
 end
