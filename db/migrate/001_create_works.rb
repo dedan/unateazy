924b50f6b0597fdce4ba2e77916055d62492e802
Stephan Gabler <stephan.gabler@gmail.com>
2010-07-08 21:31:20 +0200

    fixed link to thumbnails in works

--- db/migrate/001_create_works.rb
+++ db/migrate/001_create_works.rb
@@ -1,24 +1,24 @@
 class CreateWorks < ActiveRecord::Migration
   def self.up
     create_table :works do |t|
-      t.column :headline,		  :string
-      t.column :description,	  :text
-      t.column :file_name,		  :string
-		t.column :content_type,	  :string
-		t.column :other_content,  :text
+      t.column :headline,       :string
+      t.column :description,    :text
+      t.column :file_name,      :string
+      t.column :content_type,   :string
+      t.column :other_content,  :text
 
       t.timestamps
     end
 
-	 execute "CREATE TABLE works_rel_works (
-					 work_id INT,
-					 rel_work_id INT,
-					 PRIMARY KEY(work_id, rel_work_id)
-				 );"
+   execute "CREATE TABLE works_rel_works (
+           work_id INT,
+           rel_work_id INT,
+           PRIMARY KEY(work_id, rel_work_id)
+         );"
   end
 
   def self.down
     drop_table :works
-	 drop_table :works_rel_works
+   drop_table :works_rel_works
   end
 end
